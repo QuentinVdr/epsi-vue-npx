2ec7dfbb2b0b2f446062d5cab052672ab703330b
Quentin Verdier <quent1.verdier@gmail.com>
2023-12-21 11:25:59 +0100

    Display all pokemon information (quentin & lucas)

--- src/components/PokemonAbout.vue
+++ src/components/PokemonAbout.vue
@@ -1,6 +1,19 @@
 <template>
-  <div class="pokemonDetail">
-    <h1>This is an detail page {{ id }}</h1>
+  <div class="pokemonDetail" v-if="isLoading">
+    <p>loading ...</p>
+  </div>
+  <div class="pokemonDetail" v-if="handleError">
+    <p>{{ handleError.message }}</p>
+  </div>
+  <div class="pokemonDetail" v-if="pokemon">
+    <h1>{{ pokemon.name }}</h1>
+    <div v-if="pokemon.sprites">
+      <img :src="pokemon.sprites.front_default" :alt="pokemon.name" />
+    </div>
+    <p v-for="typeInfo in pokemon.types">
+      {{ typeInfo.type.name }}
+    </p>
+    <p v-for="statInfo in pokemon.stats">{{ statInfo.stat.name }}: {{ statInfo.base_stat }}</p>
   </div>
 </template>
 
@@ -25,10 +38,10 @@ export default {
   },
   methods: {
     async getPokemon() {
-      console.log('getPokemon')
       axios
         .get(apiURL + this.id) // Use this.id instead of id
         .then((result) => {
+          console.log(result.data)
           this.pokemon = result.data
           this.isLoading = false
         })
