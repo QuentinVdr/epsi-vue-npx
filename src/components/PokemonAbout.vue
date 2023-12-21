fdcd443321d6ea5a31d29646b2e0f84868cc8a00
Quentin Verdier <quent1.verdier@gmail.com>
2023-12-21 11:08:21 +0100

    Add API request (quentin & lucas)

--- src/components/PokemonAbout.vue
+++ src/components/PokemonAbout.vue
@@ -5,12 +5,41 @@
 </template>
 
 <script>
+import axios from 'axios'
+
+const apiURL = 'https://pokeapi.co/api/v2/pokemon/'
+
 export default {
   props: {
     id: {
       type: String,
       required: true
     }
+  },
+  data() {
+    return {
+      isLoading: true,
+      handleError: null,
+      pokemon: {}
+    }
+  },
+  methods: {
+    async getPokemon() {
+      console.log('getPokemon')
+      axios
+        .get(apiURL + this.id) // Use this.id instead of id
+        .then((result) => {
+          this.pokemon = result.data
+          this.isLoading = false
+        })
+        .catch((error) => {
+          this.handleError = error // Assign error to handleError
+          this.isLoading = false
+        })
+    }
+  },
+  beforeMount() {
+    this.getPokemon()
   }
 }
 </script>
