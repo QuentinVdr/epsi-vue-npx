7caa2d1788eeb2a707ff0d99f073c2e1436cf38d
furkan <zarodza49@gmail.com>
2023-12-21 10:53:31 +0100

    composant carte pokemon

---
+++ src/components/PokemonCard.vue
@@ -0,0 +1,31 @@
+<!-- Component PokemonCard.vue -->
+<template>
+    <div class="pokemon-card">
+      <img :src="pokemon.image" :alt="pokemon.name" class="pokemon-image" />
+      <div class="pokemon-info">
+        <h2>{{ pokemon.name }}</h2>
+        <p>Type: {{ pokemon.type }}</p>
+        <p>Height: {{ pokemon.height }}</p>
+        <p>Weight: {{ pokemon.weight }}</p>
+      </div>
+    </div>
+  </template>
+
+  <script>
+  export default {
+    props: {
+      pokemon: {
+        type: Object,
+        required: true,
+      },
+    },
+  };
+  </script>
+
+  <style scoped>
+  .pokemon-card {
+    border: 1px solid #ddd;
+    padding: 10px;
+    margin: 10px;
+    text-align: center;
+  }
